96449defa73429379562c71345cfc2ac1b7d25e7
Xiao-Lii <63008453+Xiao-Lii@users.noreply.github.com>
2020-10-31 00:01:17 -0600

    HOUSTON WE HAVE A MULTI-THREADED SERVER W/ COMMENTS

--- src/server/MultiThreadServer.java
+++ src/server/MultiThreadServer.java
@@ -4,9 +4,9 @@ import java.net.Socket;
 import java.io.IOException;
 
 public class MultiThreadServer implements Runnable {
-    protected int          serverPort   = 8080;
+    protected int          serverPort   = 10000;
     protected ServerSocket serverSocket = null;
-    protected boolean      isStopped    = false;
+    protected boolean isServerClosed = false;
     protected Thread       runningThread= null;
 
     public MultiThreadServer(int port){
@@ -14,49 +14,47 @@ public class MultiThreadServer implements Runnable {
     }
 
     public void run(){
+        // SETTING UP MAIN THREAD
         synchronized(this){
             this.runningThread = Thread.currentThread();
         }
-        openServerSocket();
-        while(! isStopped()){
-            Socket clientSocket = null;
-            try {
-                clientSocket = this.serverSocket.accept();
-            } catch (IOException e) {
-                if(isStopped()) {
-                    System.out.println("Server Stopped.") ;
-                    return;
-                }
-                throw new RuntimeException(
-                        "Error accepting client connection", e);
-            }
-            new Thread(
-                    new clientWorker(
-                            clientSocket, "Multithreaded Server")
-            ).start();
-        }
-        System.out.println("Server Stopped.") ;
-    }
 
-
-    private synchronized boolean isStopped() {
-        return this.isStopped;
-    }
-
-    public synchronized void stop(){
-        this.isStopped = true;
-        try {
-            this.serverSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Error closing server", e);
-        }
-    }
-
-    private void openServerSocket() {
+        // OPENING THE SERVER SOCKET
         try {
             this.serverSocket = new ServerSocket(this.serverPort);
         } catch (IOException e) {
-            throw new RuntimeException("Cannot open port 8080", e);
+            throw new RuntimeException("Error: Failed to open port #: " + this.serverPort, e);  // MAY NEED TO EDIT THIS
+        }
+
+        // WHILE THE SERVER ISN'T CLOSE - EXECUTE FOLLOWING EMBEDDED CODE
+        // CREATE A NEW THREAD FOR EACH CLIENT CONNECTION - EACH HAVE THEIR OWN INPUT AND OUTPUT
+        while(!isServerClosed()){
+            Socket clientSocket = null;
+
+            // ATTEMPT TO ACCEPT CLIENT CONNECTION
+            try {clientSocket = this.serverSocket.accept(); } catch (IOException e) {
+                if(isServerClosed()) {
+                    System.out.println("Server Stopped.") ;
+                    return;
+                }
+                throw new RuntimeException("Error accepting client connection", e);
+            }
+            // CREATES NEW THREAD & STARTS CLIENT WORKER
+            new Thread(new clientWorker(clientSocket, "Multithreaded Server")).start();
+        }
+        // SERVER SHUTDOWN - DONE WITH CLIENT CONNECTIONS
+        System.out.println("The server will now shut down.\n") ;
+    }
+
+
+    private synchronized boolean isServerClosed() {
+        return this.isServerClosed;
+    }
+
+    public synchronized void stop(){
+        this.isServerClosed = true;
+        try { this.serverSocket.close(); } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
         }
     }
 
--- src/tempMain.java
+++ src/tempMain.java
@@ -1,15 +1,3 @@
-
-/*
-
- This company wants a system that allows their employees to manage the products offered in the store as well
- the users and product categories. Moreover, they want an application for their customers to browse their catalog,
- search for products and make orders.
-
-
-
- */
-
-
 import Users.DepartmentStore;
 import server.MultiThreadServer;
 
