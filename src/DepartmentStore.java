050bc3312c96fc2de5d65ead113c50ea9f55638d
rinv12 <58483285+rinv12@users.noreply.github.com>
2020-10-31 23:58:45 -0600

    HOW TO CHECK PROD AVAIL??
    Do we have a method to check product availability??

--- src/DepartmentStore.java
+++ src/DepartmentStore.java
@@ -1,9 +1,6 @@
 import Product.Category;
 import Product.Product;
-import Users.Admin;
-import Users.Customer;
-import Users.Order;
-import Users.User;
+import Users.*;
 import com.sun.org.apache.xpath.internal.operations.Or;
 
 import java.security.SecureRandom;
@@ -12,8 +9,7 @@ import java.util.*;
 public class DepartmentStore{
     private ArrayList<Category> listOfCategories; //electronics
     private ArrayList<User> listOfUsers; // list of all users
-    private ArrayList<Order> listOfCustOrders; // list of all the orders -> list of list
-    // move listOfCustOrders to Customer class
+   // private ArrayList<Order> listOfCustOrders; // moved to Customers class
 
     public void createUser(String type, String email, String password, String displayName) {
         if(type.equalsIgnoreCase("Users.Admin") || type.equalsIgnoreCase("Administrator"))
@@ -28,44 +24,48 @@ public class DepartmentStore{
 
 
     //Customer's credentials should already been authenticated
-    public void newOrder(Customer dName, Product p) {
+    public void newOrder(Customer c, Product p) {
         SecureRandom random = new SecureRandom();//order#
         Order o = null;
 
         for (Iterator<User> us = listOfUsers.iterator(); us.hasNext(); ) {
             //if user has an account...
-            if (listOfUsers.contains(dName)) {
+            if (listOfUsers.contains(c)) {
                 //if user has a open order
-                for (Iterator<Order> or = listOfCustOrders.iterator(); or.hasNext(); ) {
+                for (Iterator<Order> or = c.getListOfCustOrders().iterator(); or.hasNext(); ) {
                     o.getOrderNum();
-                    addProduct(dName, p, o);
+                    addProduct(c, p, o);
                 }
             }else {//user with no open orders will get an order number +
                 o.setOrderNum(Integer.valueOf(String.valueOf(random)));
-                addProduct(dName, p, o);
+                addProduct(c, p, o);
             }
         }
     }
 
-    public void addProduct(Customer dName, Product p, Order o){
+    public void addProduct(Customer c, Product p, Order o){
+        StoreSystem sp = null;
         for (Iterator<User> us = listOfUsers.iterator(); us.hasNext();) {
-            if(listOfUsers.contains(dName)){//if user is in listsOfUsers
-                for (Iterator<Order> or= listOfCustOrders.iterator(); or.hasNext();){
-                    if (listOfCustOrders.contains(o.getOrderNum())){//if user has order number
-                      //  o.getProdsInOrder().add(p.getProductName());// how to add product?
-                    }
+            if(listOfUsers.contains(c)){//if user is in listsOfUsers
+
+                //need to figure out how to check prod availability
+                for (Iterator<Order> or= c.getListOfCustOrders().iterator(); or.hasNext();){
+                    if (c.getListOfCustOrders().contains(o.getOrderNum())){//if user has order number
+                      // if(sp.getProduct(p.getProductID(), p.getProductName()) == IS AVAILABLE! )//how to find out prod avail?
+                      o.getProdsInOrder().add(sp.getProduct(p.getProductID(), p.getProductName()));// prod will be added
+                    }//else{
+                    //prod not available!!!
+
                 }
             }
 
             }
         }
-        //iterate through listCustOfOrder
-        //check if propsInProduct contains p
 
-    public void removeProduct(Customer dName, Product p){
+    public void removeProduct(Customer c, Product p){
         for (Iterator<User> us = listOfUsers.iterator(); us.hasNext();) {
-            if (listOfUsers.contains(dName)){
-                for (Iterator<Order> or = listOfCustOrders.iterator(); or.hasNext(); ) {
+            if (listOfUsers.contains(c)){
+                for (Iterator<Order> or = c.getListOfCustOrders().iterator(); or.hasNext(); ) {
                     if (or.next().getProdsInOrder().contains(p.getProductName())) {
                     or.remove();
                     }
@@ -74,12 +74,20 @@ public class DepartmentStore{
         }
     }
 
+    //counts list of orders... will change if needed
+    public void countDuplicateItems(Customer c){
+        Set<Order> orderList = new HashSet<Order>(c.getListOfCustOrders());
+        for (Order temp : orderList){
+            System.out.println(temp + ": " + Collections.frequency(orderList, temp));
+        }
+    }
+
     //is this right???
-    public Order finalizeOrder(Customer dName, Order o){
+    public Order finalizeOrder(Customer c, Order o){
         for(Iterator<User> u = listOfUsers.iterator(); u.hasNext();){//can I delete this?
-            if(listOfUsers.contains(dName)){
-                for(Iterator<Order> or = listOfCustOrders.iterator(); or.hasNext();){
-                    if(listOfCustOrders.contains(o.getOrderNum())){
+            if(listOfUsers.contains(c)){
+                for(Iterator<Order> or = c.getListOfCustOrders().iterator(); or.hasNext();){
+                    if(c.getListOfCustOrders().contains(o.getOrderNum())){
                          o.setStatus(true);// is this right?
                     }else{
                         o.setStatus(false);
@@ -91,10 +99,10 @@ public class DepartmentStore{
         return o;
     }
 
-    public void cancelOrder(Customer dName, Order o){
-        if(listOfUsers.contains(dName)){
-            if(listOfCustOrders.contains(o.getOrderNum())){
-                listOfCustOrders.remove(o.getOrderNum());
+    public void cancelOrder(Customer c, Order o){
+        if(listOfUsers.contains(c)){
+            if(c.getListOfCustOrders().contains(o.getOrderNum())){
+                c.getListOfCustOrders().remove(o.getOrderNum());
             }
         }
     }
