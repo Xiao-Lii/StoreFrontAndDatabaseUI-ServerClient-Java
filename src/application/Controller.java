d0d76dd797f6a7a4cb6413ace3d42ac3a66980da
Rizzul Karki <ryanrizzulkarki@yahoo.com>
2020-10-31 21:48:19 -0600

    basic login screen

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,12 +1,47 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import server.*;
 
-public class Controller {
+import javax.xml.soap.Text;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller{
     Client client;
+
+    public Button loginButton;
+    public TextField usernameTextField;
+    public TextField passwordTextField;
+    public Label loginLabel;
+    public Label loginConfirmLabel;
 
     public Controller(){
         client = new Client();
         client.connect();
+    }
+
+    public void login(javafx.event.ActionEvent actionEvent) throws Exception {
+        if(usernameTextField.getText().equals("username") && passwordTextField.getText().equals("password")) {
+            loginConfirmLabel.setText("Login Successful.");
+            loginConfirmLabel.setTextFill(Color.GREEN);
+
+            Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("Department Store Application");
+            primaryStage.setScene(new Scene(root, 1400, 800));
+            primaryStage.show();
+        }else{
+            loginConfirmLabel.setText("Username or password incorrect.");
+            loginConfirmLabel.setTextFill(Color.RED);
+        }
     }
 }
